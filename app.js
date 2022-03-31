f8d5fe5d1b0963ad722c98da31ab2e5e4d3592c1
Vazquez Burgos <24jvazquezburgos@FVFZMYH2J1WK.tld>
2022-03-31 13:02:32 -0500

    Unidad doce

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 function run() {
-    alert("Helllo")
+    document.getElementById("paragraph").innerHTML = "Hello";
+    document.getElementById("paragraph").style.backgroundColor = "green";
+    document.getElementById("paragraph").style.color = "red";
+    document.getElementById("paragraph").style.padding = "20px";
+    randomNum();
 }
+
+
+
+
+function randomNum() {
+    let ran = Math.floor(Math.random() * 10);
+    let x = document.getElementById("random")
+    x.innerHTML = ran;
+    x.style.backgroundColor = "purple";
+    x.style.color = "red";
+    x.style.padding = "20px";
+    x.style.textAlign = "center";
+
+}
+
+function
