251081eb98b5cf51973df5589a0ab9659747d6bd
Vazquez Burgos <24jvazquezburgos@FVFZMYH2J1WK.tld>
2022-04-08 11:42:53 -0500

    unit12updated

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+
+
+let userNum = document.getElementById("getNumber");
+userNum.addEventListener("keydown", function (e){
+    if (e.keyCode === 13) {
+        run(e);   
+    }
+});
+
+userNum.focus();
+
+let counter = 0;
+
+
 function run() {
-    document.getElementById("paragraph").innerHTML = "Hello";
-    document.getElementById("paragraph").style.backgroundColor = "green";
-    document.getElementById("paragraph").style.color = "red";
-    document.getElementById("paragraph").style.padding = "20px";
+    
     randomNum();
+    userNumber();
+    compareNumbers();
 }
 
 
@@ -11,13 +24,69 @@ function run() {
 
 function randomNum() {
     let ran = Math.floor(Math.random() * 10);
-    let x = document.getElementById("random")
-    x.innerHTML = ran;
-    x.style.backgroundColor = "purple";
-    x.style.color = "red";
-    x.style.padding = "20px";
-    x.style.textAlign = "center";
-
+    return ran
 }
 
-function
+
+
+function userNumber() {
+    let user = document.getElementById("getNumber").value;
+    return user
+}
+
+
+function compareNumbers() {
+    let a = userNumber();
+    let b = randomNum();
+    let z = document.getElementById("compare")
+    let c = document.getElementById("counter");
+
+    if(a!= b){
+        z.innerHTML = "Numbers are not the same, Computer got"  + b + ",and user got" + a ;
+        z.style.color = "white";
+        z.style.backgroundColor = "red";
+        z.style.padding = "15px";
+        z.style.textAlign = "center";
+        counter++;
+        c.innerHTML = " You have tried " + counter + "times"
+        c.style.color = "white";
+        c.style.backgroundColor = "blue";
+        c.style.padding = "15px";
+        c.style.textAlign = "center";
+        document.querySelector("body").style.backgroundColor = "#712320"
+    }else if (a == b) {
+        z.innerHTML = "Numbers are the same, Computer got" + b + ",and user got" + a ;
+        z.style.color = "white";
+        z.style.backgroundColor = "green";
+        z.style.padding = "15px";
+        z.style.textAlign = "center";
+        c.innerHTML = " You have tried " + counter + "times to get it right"
+        c.style.color = "white";
+        c.style.backgroundColor = "blue";
+        c.style.padding = "15px";
+        c.style.textAlign = "center"; 
+        counter = 0;
+        document.querySelector("body").style.backgroundColor = "#327569"
+
+    }
+
+    resetInput();
+}
+
+
+function resetInput(){
+    document.getElementById("getNumber").value = "";
+}
+
+
+
+
+// function userNumber() {
+//     let user = document.getElementById("getNumber").value;
+//     let y = document.getElementById("userNumber");
+//     y.innerHTML = user;
+//     y.style.color = "white"
+//     y.style.backgroundColor = "orange";
+//     y.style.padding = "20px";
+//     y.style.textAlign = "center";
+// }
